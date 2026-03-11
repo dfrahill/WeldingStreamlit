3d410d40519bbe8dccbeca09ee5f8f0c43aef50d
dfrahill <danfrahill@gmail.com>
2026-03-11 16:10:55 -0700

    Minor Test if it commits.

--- WeldingStreamlit.py
+++ WeldingStreamlit.py
@@ -1,4 +1,4 @@
-#from matplotlib.ticker import scale_range
+#from matplotlib.ticker import scale_range (testing GIT Update)
 from pydoc import plainpager
 import streamlit as st
 import pandas as pd
