8f928dc0e8b6a9fb7bcc3ce4da296cf469b36cdd
dfrahill <danfrahill@gmail.com>
2026-03-11 15:56:29 -0700

    Update from new laptop

--- WeldingStreamlit.py
+++ WeldingStreamlit.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-import plotly.express as px
+import plotly.exp ress as px
 from datetime import datetime, timedelta
 import io
 
